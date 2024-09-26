e400e710fb29622ac2bfff3e53210b23127ec9a1
Devesh Kumar <deveshjha46@gmail.com>
2024-09-26 20:56:06 +0530

    Debugging

--- src/middlewares/auth.miidleware.js
+++ src/middlewares/auth.miidleware.js
@@ -1,12 +1,12 @@
-import { User } from "../models/user.model";
-import { ApiError } from "../utils/APIerrors";
-import { asyncHandler } from "../utils/asynchHandler";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/APIerrors.js";
+import { asyncHandler } from "../utils/asynchHandler.js";
 import jwt from "jsonwebtoken"
 
 export const verifyToken = asyncHandler(async(req,res,next)=>{
      try {
           const token = req.cookies?.accessToken || req.header
-          ("Authorization")?.replace("Bearer ","");    // this is from jwt
+          ("Authorization")?.replace("Bearer ","");    // this is from jwt 
          
           if(!token){
                throw new ApiError(401,"Unauthorized request");
